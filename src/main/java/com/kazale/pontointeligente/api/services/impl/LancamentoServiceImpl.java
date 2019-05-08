0889f7b225845411cc0d8134ec24d0e2c68f598d
bruno <brunoyuzuki@yahoo.com.br>
2019-05-08 01:39:14 -0300

    Adicionado EhCache

--- src/main/java/com/kazale/pontointeligente/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/kazale/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -2,8 +2,11 @@ package com.kazale.pontointeligente.api.services.impl;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -15,7 +18,7 @@ import com.kazale.pontointeligente.api.services.LancamentoService;
 @Service
 public class LancamentoServiceImpl implements LancamentoService {
 	
-	private static final org.slf4j.Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
+	private static final Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
 	
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
@@ -25,11 +28,13 @@ public class LancamentoServiceImpl implements LancamentoService {
 		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
 	}
 	
+	@Cacheable("lancamentoPorId")
 	public Optional<Lancamento> buscarPorId(Long id) {
 		log.info("Buscando um lançamento pelo ID {}", id);
 		return this.lancamentoRepository.findById(id);
 	}
 	
+	@CachePut("lancamentoPorId")
 	public Lancamento persistir(Lancamento lancamento) {
 		log.info("Persistindo o lançamento: {}", lancamento);
 		return this.lancamentoRepository.save(lancamento);
